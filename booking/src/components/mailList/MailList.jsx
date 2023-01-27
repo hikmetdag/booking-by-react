9fa5fee9239929a7cd1fb11b39014b731709a4fb
Hikmet Dag <temkihgad@gmail.com>
2023-01-27 13:49:28 +0100

    commit

---
+++ booking/src/components/mailList/MailList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './mailList.css'
+
+const MailList = () => {
+  return (
+    <div className='mail'>
+      <h1 className="mailTitle">Save time, save money!</h1>
+      <span className="mailDesc">Sign up and we'll send the best deals to you</span>
+      <div className="mailInput">
+        <input type="text  " placeholder='Your email adress'/>
+        <button>Subscribe</button>
+      </div>
+    </div>
+  )
+}
+
+export default MailList
---
+++ booking/src/components/navbar/navbar.jsx
@@ -0,0 +1,17 @@
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbarConatainer">
+        <span className="logo">Mune logo</span>
+        <div className="navitems">
+          <button className="navButton">Register</button>
+          <button className="navButton">Register</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
